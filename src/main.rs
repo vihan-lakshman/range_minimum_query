d88670feda6b167cdd637db64a58118ae8409a85
Vihan Lakshman <vihan@Vihans-MacBook-Pro.local>
2020-04-19 19:10:17 -0700

    Add basic dynamic programming solution

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::cmp;
 use std::io;
 
 fn main() {
@@ -6,9 +7,12 @@ fn main() {
     let left_bound = prompt_for_bound(true);
     let right_bound = prompt_for_bound(false);
 
-    let range_minimum = brute_force_rmq(array, left_bound, right_bound);
+    let bf_range_minimum = brute_force_rmq(array, left_bound, right_bound);
+    let dp_range_minimum = dp_rmq(array, left_bound, right_bound);
 
-    println!("The minimum is {}", range_minimum);
+    assert_eq!(bf_range_minimum, dp_range_minimum);
+
+    println!("The minimum is {}", dp_range_minimum);
 }
 
 fn prompt_for_bound(is_left: bool) -> usize {
@@ -29,6 +33,8 @@ fn prompt_for_bound(is_left: bool) -> usize {
     bound
 }
 
+// Preprocessing Time: O(1)
+// Query Time: O(n)
 fn brute_force_rmq(array: [i32; 9], left_bound: usize, right_bound: usize) -> i32 {
     let mut minimum = array[left_bound];
 
@@ -39,4 +45,28 @@ fn brute_force_rmq(array: [i32; 9], left_bound: usize, right_bound: usize) -> i3
     }
 
     minimum
+}
+
+// Preprocessing Time: O(n^2) (via dynamic programming)
+// Query Time: O(1)
+fn dp_rmq(array: [i32; 9], left_bound: usize, right_bound: usize) -> i32 {
+    let mut minimum_value;
+    let mut dp_rmq_structure = vec![vec![100; 9]; 9];
+    
+    // Initial linear pass over the array
+    // Complexity: O(n)
+    for i in 0..array.len() {
+        dp_rmq_structure[i][i] = array[i];
+    }
+
+    // Secondary pass to fill in the dp rmq structure via dynamic programming
+    // Complexity: O(n^2)
+    for i in 0..array.len() {
+        for j in i+1..array.len() {
+            minimum_value = cmp::min(dp_rmq_structure[i][j-1], dp_rmq_structure[j][j]);
+            dp_rmq_structure[i][j] = minimum_value;
+        }
+    }
+
+    dp_rmq_structure[left_bound][right_bound]
 }
